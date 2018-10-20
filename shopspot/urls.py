40e90c2cc3c1f907207f4f633dc5810b71bd059c
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-10-20 13:52:07 +0500

    Add user api views and urls for router

--- shopspot/urls.py
+++ shopspot/urls.py
@@ -18,6 +18,24 @@ from django.urls import path
 from django.conf.urls import include
 
 from django.views.generic import TemplateView
+from accounts.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 
 urlpatterns = [
@@ -28,4 +46,8 @@ urlpatterns = [
     path('accounts/profile/',
         TemplateView.as_view(template_name='profile.html'),
         name='profile'),
+    path('api-auth/',
+        include('rest_framework.urls')
+    ),
+    path('api/', include(router.urls))
 ]
